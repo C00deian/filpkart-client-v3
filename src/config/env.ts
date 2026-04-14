8f47ad932b51ac11c1080fd81de66c3e173af8ae
Ritik kumar <ritikkumarkashyap9@gmail.com>
2026-04-14 13:45:22 +0530

    deployment

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,8 @@
 export const ENV = {
-  API_URL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080',
-  CLOUDINARY_CLOUD: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME ?? '',
-  CLOUDINARY_PRESET: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET ?? '',
-} as const
+  // API_URL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080',
+  API_URL:
+    import.meta.env.VITE_API_URL ??
+    "https://flipkart-style-ecommerce-backend.onrender.com",
+  CLOUDINARY_CLOUD: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME ?? "",
+  CLOUDINARY_PRESET: import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET ?? "",
+} as const;
